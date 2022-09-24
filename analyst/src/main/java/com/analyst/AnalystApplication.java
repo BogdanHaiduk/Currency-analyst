e7d6e63429051747f54530948df38239dcbad192
Bohdan <haidukbogdan@gmail.com>
2022-09-24 18:39:00 +0300

    created 2 packages for MS Analyst and Exchanges

---
+++ analyst/src/main/java/com/analyst/AnalystApplication.java
@@ -0,0 +1,11 @@
+package com.analyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalystApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnalystApplication.class, args);
+    }
+}
---
+++ exchanges/src/main/java/com/exchanges/ExchangesApplication.java
@@ -0,0 +1,11 @@
+package com.exchanges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangesApplication.class, args);
+    }
+}
